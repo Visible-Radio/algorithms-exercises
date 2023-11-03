df5c304df63256641f01809f8c4ff1550fffe7b2
Patrick Kaipainen <patrick.kaipainen@gmail.com>
2023-11-02 21:37:21 -0400

    dur dur dur

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -6,13 +6,71 @@
   You'll need to write more than just one function
 */
 
-const mergeSort = (nums) => {
-  // code goes here
+const merge = (a, b) => {
+  let result = [];
+  let cursor1 = 0;
+  let cursor2 = 0;
+
+  // when a number is added to result, increment the cursor for the source array it came from
+  while (cursor1 < a.length && cursor2 < b.length) {
+    const elementA = a[cursor1];
+    const elementB = b[cursor2];
+
+    if (elementA < elementB || elementA === elementB) {
+      result.push(elementA);
+      cursor1++;
+    } else if (elementB < elementA) {
+      result.push(elementB);
+      cursor2++;
+    }
+  }
+
+  // finish up
+  // if one of the arrays had 'left overs', append those to the end of result
+  const leftOversA = a.slice(cursor1);
+  const leftOversB = b.slice(cursor2);
+
+  return [...result, ...leftOversA, ...leftOversB];
 };
+
+const mergeSort = nums => {
+  if (nums.length === 1) return nums;
+
+  const middle = Math.ceil(nums.length / 2);
+  const a = nums.slice(0, middle);
+  const b = nums.slice(middle);
+
+  return merge(mergeSort(a), mergeSort(b));
+};
+
+describe("merge function", () => {
+  const mergeCases = [
+    [[38], [27], [27, 38]],
+    [
+      [27, 38],
+      [3, 43],
+      [3, 27, 38, 43],
+    ],
+    [[9, 82], [10], [9, 10, 82]],
+    [
+      [3, 27, 38, 43],
+      [9, 10, 82],
+      [3, 9, 10, 27, 38, 43, 82],
+    ],
+  ];
+
+  mergeCases.forEach(([a, b, expectedResult]) => {
+    test(`merge([${a.join(", ")}], [${b.join(
+      ", "
+    )}]) yields [${expectedResult.join(", ")}]`, () => {
+      expect(merge(a, b)).toEqual(expectedResult);
+    });
+  });
+});
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
